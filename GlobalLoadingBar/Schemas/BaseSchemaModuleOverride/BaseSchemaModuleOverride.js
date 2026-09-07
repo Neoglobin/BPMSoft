833559ee1368fb9332db1ee7670240f31716fd64
Neo <levinalex1111@gmail.com>
2026-09-06 23:45:21 -0700

    Добавлена загрузка по сис-настройке. Привязаны данные

--- GlobalLoadingBar/Schemas/BaseSchemaModuleOverride/BaseSchemaModuleOverride.js
+++ GlobalLoadingBar/Schemas/BaseSchemaModuleOverride/BaseSchemaModuleOverride.js
@@ -3,17 +3,28 @@ define("BaseSchemaModuleOverride", ["LoadingBarModule"], function() {
 		override: "BPMSoft.configuration.BaseSchemaModule",
 		alternateClassName: "BPMSoft.BaseSchemaModuleOverride",
 
+		isLoadingBarActive: false,
+
 		init: function() 
 		{
-			BPMSoft.LoadingBarModule.init();
-			BPMSoft.LoadingBarModule.show();
+			BPMSoft.SysSettings.querySysSettingsItem("GlobalLoadingBarActive", (isActive) => {
+				if (isActive) 
+				{
+					this.isLoadingBarActive = isActive;
+					BPMSoft.LoadingBarModule.init();
+					BPMSoft.LoadingBarModule.show();
+				}
+			}, this);
 			this.callParent(arguments);
 		},
 
 		render: function() 
 		{
 			this.callParent(arguments);
-			BPMSoft.LoadingBarModule.hide();
+			if (this.isLoadingBarActive) 
+			{
+				BPMSoft.LoadingBarModule.hide();
+			}
 		}
 	});
 	return BPMSoft.BaseSchemaModuleOverride;

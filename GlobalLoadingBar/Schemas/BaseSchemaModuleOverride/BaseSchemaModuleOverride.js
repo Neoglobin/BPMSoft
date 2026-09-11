f41f1d0bb641de6c5922a1896c450922de61d896
Neo <levinalex1111@gmail.com>
2026-09-11 00:16:47 -0700

    Поправлены ошибки. Добавлен предохранитель от бесконечной загрузки. Добавлена настройка порогового значения отрисовки

--- GlobalLoadingBar/Schemas/BaseSchemaModuleOverride/BaseSchemaModuleOverride.js
+++ GlobalLoadingBar/Schemas/BaseSchemaModuleOverride/BaseSchemaModuleOverride.js
@@ -8,7 +8,8 @@ define("BaseSchemaModuleOverride", ["LoadingBarModule"], function() {
 		isModuleInitialized: false,
 		intervalId: null,
 		delay: 0,
-		delayEdge: 0,
+		loadingBarRenderDelayEdge: 0,
+		loadingBarDestroyDelayEdge: 0,
 		
 
 		init: function(callback, scope) 
@@ -26,31 +27,47 @@ define("BaseSchemaModuleOverride", ["LoadingBarModule"], function() {
 				callback.call(scope || this);
 			}, this]);
 
-			if (!this.intervalId && !this.isModuleInitialized) 
+			if (this.isLoadingBarActive) 
 			{
-				BPMSoft.SysSettings.querySysSettingsItem("GlobalLoadingBarRenderDelayEdge", (edgeValue) => {
-					if (edgeValue) 
-					{
-						this.delayEdge = edgeValue;
-					}
-				}, this);
-
-				this.intervalId = setInterval(() => {
-
-					if (this.isModuleInitialized) 
-					{
-						clearInterval(this.intervalId);
-						return;
-					}
+				if (!this.intervalId && !this.isModuleInitialized) 
+				{
+					BPMSoft.SysSettings.querySysSettingsItem("GlobalLoadingBarRenderDelayEdge", (edgeValue) => {
+						if (edgeValue) 
+						{
+							this.loadingBarRenderDelayEdge = edgeValue;
+						}
+					}, this);
+					BPMSoft.SysSettings.querySysSettingsItem("GlobalLoadingBarDestroyDelayEdge", (edgeValue) => {
+						if (edgeValue) 
+						{
+							this.loadingBarDestroyDelayEdge = edgeValue;
+						}
+					}, this);
 	
-					if (!this.isLoadingBarRendered && this.delay >= this.delayEdge) 
-					{
-						BPMSoft.LoadingBarModule.show();
-						this.isLoadingBarRendered = true;
-					}
-					
-					this.delay += 100; 
-				}, 100);
+					this.intervalId = setInterval(() => {
+	
+						if (this.isModuleInitialized || this.delay >= this.loadingBarDestroyDelayEdge) 
+						{
+							clearInterval(this.intervalId);
+
+							if (this.delay >= this.loadingBarDestroyDelayEdge) 
+							{
+								BPMSoft.LoadingBarModule.hide();
+								this.isLoadingBarRendered = false;
+							}
+
+							return;
+						}
+		
+						if (!this.isLoadingBarRendered && this.delay >= this.loadingBarRenderDelayEdge) 
+						{
+							BPMSoft.LoadingBarModule.show();
+							this.isLoadingBarRendered = true;
+						}
+						
+						this.delay += 100; 
+					}, 100);
+				}
 			}
 		},
 
@@ -58,11 +75,27 @@ define("BaseSchemaModuleOverride", ["LoadingBarModule"], function() {
 		{			
 			this.callParent(arguments);
 
-			if (this.isLoadingBarRendered) 
+			if (this.isLoadingBarActive) 
 			{
-				BPMSoft.LoadingBarModule.hide();
-				this.isLoadingBarRendered = false;
+				if (this.isLoadingBarRendered) 
+				{
+					BPMSoft.LoadingBarModule.hide();
+					this.isLoadingBarRendered = false;
+				}
 			}
+		},
+
+		destroy: function() 
+		{
+			this.isLoadingBarActive = false;
+			this.isLoadingBarRendered = false;
+			this.isModuleInitialized = false;
+			this.intervalId = null;
+			this.delay = 0;
+			this.loadingBarRenderDelayEdge = 0;
+			this.loadingBarDestroyDelayEdge = 0;
+
+			this.callParent(arguments);
 		}
 	});
 	return BPMSoft.BaseSchemaModuleOverride;
